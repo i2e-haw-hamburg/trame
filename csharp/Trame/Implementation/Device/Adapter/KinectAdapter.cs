3b80269c6e371562688b3ab9fa810b086b4db9ff
Jonathan Wischhusen <jonathan.wischhusen@gmail.com>
2015-04-20 13:40:05 +0200

    added kinect smoothing parameter at device initialization

--- csharp/Trame/Implementation/Device/Adapter/KinectAdapter.cs
+++ csharp/Trame/Implementation/Device/Adapter/KinectAdapter.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Kinect;
 
 namespace Trame.Implementation.Device.Adapter
@@ -20,7 +24,14 @@ namespace Trame.Implementation.Device.Adapter
             }
             kinect.SkeletonFrameReady += onFrameArrived;
             kinect.Start();
-            kinect.SkeletonStream.Enable();
+            kinect.SkeletonStream.Enable(new TransformSmoothParameters()
+            {
+                Smoothing = 0.75f,
+                Correction = 0.0f,
+                Prediction = 0.0f,
+                JitterRadius = 0.05f,
+                MaxDeviationRadius = 0.04f
+            });
         }
 
         public void StopKinect()
