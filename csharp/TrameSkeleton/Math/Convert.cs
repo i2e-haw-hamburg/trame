85f188ec83cfebe704caef97d0bc1afd0a0d328a
Test PC <i2e@haw-hamburg.de>
2015-04-22 02:36:14 -0700

    Add transformation into service

--- csharp/TrameSkeleton/Math/Convert.cs
+++ csharp/TrameSkeleton/Math/Convert.cs
@@ -8,9 +8,23 @@ namespace TrameSkeleton.Math
 {
     public static class Convert
     {
+        private static Matrix4x4 TransformationMatrix{
+            get
+            {
+                var m = new Matrix4x4();
+                m.V00 = -1;
+                m.V11 = -1;
+                m.V22 = 1;
+                m.V33 = 1;
+                return m;
+            }
+        } 
+
         public static Vector3 InternalToWorldCoordinate(Vector3 vec)
         {
-            return new Vector3(-vec.X, -vec.Z, vec.Y);
+            var correctCoordinates = new Vector4(-vec.X, -vec.Z, vec.Y, 1);
+            var transformedVector = TransformationMatrix * correctCoordinates;
+            return new Vector3(transformedVector.X, transformedVector.Y, transformedVector.Z);
         }
     }
 }
