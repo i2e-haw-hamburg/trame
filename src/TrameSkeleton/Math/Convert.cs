944037b1ceeb6677d9af2c890ddca954e4f4562d
Test PC <i2e@haw-hamburg.de>
2015-06-18 15:04:50 +0200

    Change rotation of skeleton in world

--- src/TrameSkeleton/Math/Convert.cs
+++ src/TrameSkeleton/Math/Convert.cs
@@ -13,7 +13,7 @@ namespace TrameSkeleton.Math
         /// <returns></returns>
         public static Vector3 InternalToWorldCoordinate(Vector3 vec)
         {
-            return new Vector3(-vec.X, vec.Y, vec.Z) / 1000;
+            return new Vector3(vec.X, vec.Y, -vec.Z) / 1000;
         }
 
         /// <summary>
@@ -23,7 +23,7 @@ namespace TrameSkeleton.Math
         /// <returns></returns>
         public static Vector4 InternalToWorldCoordinate(Vector4 vec)
         {
-            return new Vector4(-vec.X, vec.Y, vec.Z, -vec.W);
+            return new Vector4(vec.X, vec.Y, -vec.Z, -vec.W);
         }
     }
 }
