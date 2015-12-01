73742911877c5dda340800d806cf385c0e087a38
jason <jason.wilmans@haw-hamburg.de>
2015-12-01 15:47:01 +0100

    sdfsdf

---
+++ src/TrameSkeleton/Implementation/HandJoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Trame;
+using Trame.Implementation.Skeleton;
+
+namespace TrameSkeleton.Implementation
+{
+    public class HandJoint : OrientedJoint, IHand
+    {
+        public bool IsClosed { get; set; }
+    }
+}
